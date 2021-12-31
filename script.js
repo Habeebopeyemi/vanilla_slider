299be73d742b99ffecb9fe785cf2c9bca0d09c1c
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2021-12-31 16:45:55 +0100

    started the application layer

---
+++ script.js
@@ -0,0 +1,38 @@
+let span = document.getElementsByTagName('span');
+let product = document.getElementsByClassName('product');
+let product_per_page = Math.ceil(product.length/4);
+// console.log(product_per_page)
+let length = 0;
+let move_per_click = 25.34;
+let max_move = 203;
+
+// Mobile view
+let mobile_view = window.matchMedia("(max-width: 768px)");
+// checking
+if (mobile_view.matches) {
+    move_per_click = 50.36;
+    max_move = 504;
+}
+let forward_movement = () => {
+    length = length + move_per_click;
+    if (product == 1) {
+        console.log('product is one!!!')
+        length = 0;
+    }
+    for (const item of product) {
+        if (length > max_move) { length = length - move_per_click; }
+        item.style.left = `-${length}%`;
+    }
+}
+ 
+let backward_movement = () => {
+    length = length - move_per_click
+    if (length <= 0) { length = 0; }
+    for (const item of product) {
+        if (product_per_page > 1)
+            item.style.left = `+${length}%`;
+    }
+}
+
+span[1].onclick = () => { forward_movement() }
+span[0].onclick = () => { backward_movement()}
