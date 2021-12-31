6196ccb9b70264791d605922ce3a36b8606ddcf8
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2021-12-31 17:16:08 +0100

    application layer slider complete

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ let product_per_page = Math.ceil(product.length/4);
 // console.log(product_per_page)
 let length = 0;
 let move_per_click = 25.34;
-let max_move = 203;
+let max_move = 153;
 
 // Mobile view
 let mobile_view = window.matchMedia("(max-width: 768px)");
 // checking
 if (mobile_view.matches) {
     move_per_click = 50.36;
-    max_move = 504;
+    max_move = 403;
 }
 let forward_movement = () => {
     length = length + move_per_click;
     if (product == 1) {
-        console.log('product is one!!!')
         length = 0;
     }
     for (const item of product) {
@@ -30,7 +29,7 @@ let backward_movement = () => {
     if (length <= 0) { length = 0; }
     for (const item of product) {
         if (product_per_page > 1)
-            item.style.left = `+${length}%`;
+            item.style.left = `-${length}%`;
     }
 }
 
